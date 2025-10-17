c88f86e44c3f3199588a9bf1833b79789e0f9a5b
Tausif Alam <tausifalam87654321@gmail.com>
2025-10-17 21:51:16 +0530

    Fix bug and compleat Detail Page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { useRouter, useLocalSearchParams } from "expo-router";
 import { fetchMovieDetails } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { icons } from "@/constants/icons";
@@ -10,15 +10,16 @@ interface MovieInfoProps {
   value?: string | number | null;
 }
 
-const MovieInfo = ({label, value} : MovieInfoProps) => {
+const MovieInfo = ({label, value} : MovieInfoProps) => (
   <View className="flex-col items-start justify-center mt-5">
     <Text className="text-light-200 font-normal text-sm">{label}</Text>
     <Text className="text-light-100 font-bold text-sm mt-2">{value || "N/A"}</Text>
   </View>
-}
+)
 
 const Details = () => {
   const { id } = useLocalSearchParams();
+  const router  = useRouter()
 
   const { data: movie, loading } = useFetch(() =>
     fetchMovieDetails(id as string)
@@ -64,8 +65,20 @@ const Details = () => {
 
           <MovieInfo label="Overview" value={movie?.overview} />
           <MovieInfo label="Genres" value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"} />
+
+            <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo label="Budget" value={`$${(movie?.budget ?? 0) / 1_000_000} million`} />
+            <MovieInfo label="Revenue" value={`$${Math.round((movie?.revenue ?? 0) / 1_000_000)} million `} />
+            </View>
+           
+           <MovieInfo label="Production Companies" value={movie?.production_companies?.map((c) => c.name).join(" • ") || "N/A"} />
         </View>
       </ScrollView>
+
+      <TouchableOpacity className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50" onPress={router.back}>
+        <Image source={icons.arrow} className="size-5 mr-1 mt-0.5 rotate-180" tintColor={"#fff"} />
+        <Text className="text-white font-semibold text-base">Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
